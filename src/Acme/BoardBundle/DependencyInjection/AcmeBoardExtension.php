e692e5f83b578f78eb2d135a281d7392d8f347b1
username <u@ubuntu.(none)>
2014-05-02 20:54:05 -0700

    Bug fixed

--- src/Acme/BoardBundle/DependencyInjection/AcmeBoardExtension.php
+++ src/Acme/BoardBundle/DependencyInjection/AcmeBoardExtension.php
@@ -24,5 +24,7 @@ class AcmeBoardExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('voter.yml');
+        $loader->load('twig.yml');
     }
 }
